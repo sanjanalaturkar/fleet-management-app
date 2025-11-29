f56c9736f1ad785a76fac87876742fd27b2058b9
sanjanalaturkar <sanjanalaturkar2112@gmail.com>
2025-11-29 20:05:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,54 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+let fleetData = [
+    { regNo: "MH12AB1234", driverName: "ABC", category:"Car", availabilty: "Available", imageLink: "https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D"},
+    { regNo: "MH12AB5678", driverName: "DEF", category:"Bus", availabilty: "Unavailable", imageLink: "https://www.shutterstock.com/image-illustration/image-bus-isolated-on-white-600nw-2578467979.jpg"},
+    { regNo: "MH12AB59101", driverName: "GHI", category:"Auto", availabilty: "Unavailable", imageLink: "https://cdn.bajajauto.com/-/media/bajaj-auto/new-webp/3-wheeler/savings_calculator_image_re_new.webp"},
+];
+const cardContainer = document.getElementById('fleet-cards-conatiner');
+function renderFleetCards(dataToRender){
+    if(!cardContainer) return;
+    cardContainer.innerHTML = '';
+    dataToRender.forEach(fleet => {
+        const card = document.createElement('div');
+        card.className =`fleet-card ${fleet.availabilty === 'Unavailable' ? 'unavailable': ''}`;
+        const imgSrc = fleet.imageLink && fleet.imageLink.startWith('http') ? fleet.imageLink : 'https://media.istockphoto.com/id/1445074332/photo/bright-colorful-big-rigs-semi-trucks-with-semi-trailers-standing-in-the-row-on-truck-stop.jpg?s=612x612&w=0&k=20&c=N5fVLeFT119Yv0QSH2Z9UgDXFOLW1qXHqL0p7EPkPRs=';
+        card.innerHTML = `
+        <img src="${imgSrc}" alt="${fleet.regNo}">
+        <h4>Reg No: ${fleet.regNo}</h4>
+        <p><strong>Category:</strong>${fleet.driverName}</p>
+        <p><strong>Availabilty:</strong> <span class="availability-status">${fleet.availabilty}</span></p>
+        <hr>
+        <button class="update-driver-btn" data-regno="${fleet.regNo}">Update Driver</button>
+        <button class="change-availability-btn" data-regno="${fleet.regNo}">Change Availability</button>
+        <button class="delete-vehicle-btn" data-regno="${fleet.regNo}">Delete Vehicle</button>
+        `;
+        cardContainer.appendChild(card);
+    })
+}
+function handleAddFleet() {
+    const fleetForm = document.getElementById('fleet-form');
+    if (fleetForm) {
+        fleetForm.addEventListener('submit',(e) =>{
+            e.preventDefault();
+            const newRegNo = document.getElementById('regNo').value;
+            if (fleetData.some(f => f.regNo === newRegNo)){
+                alert("Error: vehicle with this registration number already exist");
+                return;
+            }
+            const newFleet = {
+                regNo: newRegNo,
+                driverName: document.getElementById('diverName').value,
+                category: document.getElementById('category').value,
+                availabilty: document.getElementById('availability').value,
+                imageLink: document.getElementById('imageLink').value,
+            };
+            fleetData.push(newFleet);
+            applyFilters();
+            fleetForm.reset();
+            alert('Fleet Item Added Successfully');
+        })
+    }
+}
+
